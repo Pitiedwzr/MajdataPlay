1397cf519e9c66d6e0de0bc0d0222f9d79426017
Pitiedwzr <43533746+Pitiedwzr@users.noreply.github.com>
2026-09-12 03:09:44 +1200

    fix(typo): wrong method name

--- Assets/Scripts/Scenes/List/CoverListManager.cs
+++ Assets/Scripts/Scenes/List/CoverListManager.cs
@@ -510,7 +510,7 @@ namespace MajdataPlay.Scenes.List
         }
         void SetCursorInternal(string hash, bool disableAnimation, bool forceUpdate)
         {
-            if (!_currentCollection.ContainsKey(hash))
+            if (!_currentCollection.ContainsHash(hash))
             {
                 return;
             }
